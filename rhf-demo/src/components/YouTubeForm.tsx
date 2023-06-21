2920b22405194d5f05fcc92ef013540e3693ac43
Sudir Krishnaa RS <Sudir.Krishnaa@albertsons.com>
2023-06-21 15:35:07 +0530

    V-9 : Display error messages

--- rhf-demo/src/components/YouTubeForm.tsx
+++ rhf-demo/src/components/YouTubeForm.tsx
@@ -12,7 +12,9 @@ type FormValues = {
 
 const YouTubeForm = () => {
   const form = useForm<FormValues>();
-  const { register, control, handleSubmit } = form;
+  const { register, control, handleSubmit, formState } = form;
+  //errors -  For validation errors messages
+  const { errors } = formState;
 
   // FORM SUBMISSION : (3 Steps)
   // Step 1: Define the functionwhich should be called when the submit button is pressed
@@ -34,45 +36,59 @@ const YouTubeForm = () => {
 
       {/* noValidate : means This will prevent browser validation and allowing react-hook-form to handle the VALIDATIONS of the feilds */}
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          {...register("username", {
-            // Validating Required 
-            required: {
-              value: true,
-              message:  "Username is required",
-            },
-          })}
-        />
+        
+        {/* This div is just for css styling  */}
+        <div className="form-control">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              // Validating Required
+              required: {
+                value: true,
+                message: "Username is required",
+              },
+            })}
+          />
+          {/* To display validation error messages */}
+          <p className="error">{errors.username?.message}</p>
+        </div>
 
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          {...register("email", {
-            // Validating RegEx 
-            pattern: {
-              value:
-                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-              message: "Invalid email format",
-            },
-          })}
-        />
+        {/* This div is just for css styling  */}
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              // Validating RegEx
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "Invalid email format",
+              },
+            })}
+          />
+          <p className="error">{errors.email?.message}</p>
+        </div>
 
-        <label htmlFor="channel">Channel</label>
-        <input
-          type="text"
-          id="channel"
-          {...register("channel", {
-            // Validating Required 
-            required: {
-              value: true,
-              message: "Channel is required",
-            },
-          })}
-        />
+        {/* This div is just for css styling  */}
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input
+            type="text"
+            id="channel"
+            {...register("channel", {
+              // Validating Required
+              required: {
+                value: true,
+                message: "Channel is required",
+              },
+            })}
+          />
+          <p className="error">{errors.channel?.message}</p>
+        </div>
 
         <button>Submit</button>
       </form>
