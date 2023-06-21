33534b224b70175fe483e8bf22e642121abc67bc
Sudir Krishnaa RS <Sudir.Krishnaa@albertsons.com>
2023-06-21 15:17:47 +0530

    V-8 : Form Validations - Required & RegEx

--- rhf-demo/src/components/YouTubeForm.tsx
+++ rhf-demo/src/components/YouTubeForm.tsx
@@ -5,41 +5,74 @@ import { DevTool } from "@hookform/devtools";
 let renderCount = 0;
 
 type FormValues = {
-  username: string
-  email: string
-  channel: string
-}
+  username: string;
+  email: string;
+  channel: string;
+};
 
 const YouTubeForm = () => {
   const form = useForm<FormValues>();
   const { register, control, handleSubmit } = form;
 
   // FORM SUBMISSION : (3 Steps)
-      // Step 1: Define the functionwhich should be called when the submit button is pressed
-      const onSubmit = (data: FormValues) => { 
-        console.log('Form submitted', data);
-      };
+  // Step 1: Define the functionwhich should be called when the submit button is pressed
+  const onSubmit = (data: FormValues) => {
+    console.log("Form submitted", data);
+  };
 
-      // Step 2: Destructure handleSubmit in form (LINE NUM 9)
-      //    const { register, control, handleSubmit } = form;
+  // Step 2: Destructure handleSubmit in form (LINE NUM 9)
+  //    const { register, control, handleSubmit } = form;
 
-      // Step 3: Pass an onSubmit function in <form> JSX tag and pass it like
-        //  <form onSubmit={handleSubmit(onSubmit //--HINT: which we defined in step 1--// )}> (LINE NUM 26)
+  // Step 3: Pass an onSubmit function in <form> JSX tag and pass it like
+  //  <form onSubmit={handleSubmit(onSubmit //--HINT: which we defined in step 1--// )}> (LINE NUM 26)
 
   renderCount++;
   return (
     <div>
       {/* HINT: renderCount / 2 : because react strict mode renders each comeponent twice so we divide by 2 */}
       <h1>Youtube Form ({renderCount / 2})</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+
+      {/* noValidate : means This will prevent browser validation and allowing react-hook-form to handle the VALIDATIONS of the feilds */}
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            // Validating Required 
+            required: {
+              value: true,
+              message:  "Username is required",
+            },
+          })}
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register("email")} />
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            // Validating RegEx 
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            // Validating Required 
+            required: {
+              value: true,
+              message: "Channel is required",
+            },
+          })}
+        />
 
         <button>Submit</button>
       </form>
