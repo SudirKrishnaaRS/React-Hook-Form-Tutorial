c35a97ae9901700bddebd1f252c9d897345abc5f
Sudir Krishnaa RS <Sudir.Krishnaa@albertsons.com>
2023-06-21 14:38:35 +0530

    V-6 : Form State and re-renders (concept explaination)

--- rhf-demo/src/components/YouTubeForm.tsx
+++ rhf-demo/src/components/YouTubeForm.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
+let renderCount = 0;
+
 const YouTubeForm = () => {
   const form = useForm();
   const { register, control } = form;
 
+  renderCount++;
   return (
     <div>
+      {/* HINT: renderCount / 2 : because react strict mode renders each comeponent twice so we divide by 2 */}
+      <h1>Youtube Form (Re-Render Count : {renderCount / 2} )</h1>
       <form>
         <label htmlFor="username">Username</label>
         <input type="text" id="username" {...register("username")} />
@@ -22,7 +27,7 @@ const YouTubeForm = () => {
       </form>
 
       {/* For Devtool visualization */}
-      <DevTool control={control}/>
+      <DevTool control={control} />
     </div>
   );
 };
