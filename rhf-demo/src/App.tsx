d0df1f23032c5a334065cacbe0404d2f8acb0436
Sudir Krishnaa RS <Sudir.Krishnaa@albertsons.com>
2023-07-04 18:29:08 +0530

    V-29 : Yup Integration

--- rhf-demo/src/App.tsx
+++ rhf-demo/src/App.tsx
@@ -1,11 +1,15 @@
 import YouTubeForm from "./components/YouTubeForm";
 
 import "./App.css";
+import { YupYouTubeForm } from "./components/YupYouTubeForm";
 
 function App() {
   return (
     <div>
       <YouTubeForm />
+      <br /><br /><br />
+      new form below <hr />
+      <YupYouTubeForm/>
     </div>
   );
 }
